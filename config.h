f06de8ff44c088f6a262b0762ab2120f94cc2a0b
Hannes Bredberg <hannesbredberg@gmail.com>
2022-04-01 18:22:54 +0000

    Grab font from command line

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = FONT ":antialias=true:autohint=true";
 static char *font2[] = { "Inconsolata for Powerline:pixelsize=24:antialias=true:autohint=true" };
 static int borderpx = 2;
 
